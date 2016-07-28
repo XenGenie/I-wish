291897cd2be45730bffae3b2e0b981036439d3bd
xopherdeep <heylisten@xtiv.net>
2016-07-27 22:04:40 -0700

    update readme

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-Cloud-Strife
-============
-Take the Headache out of setting up WebProjects.
-Cloud-Strife is a Skeleton Repo for SuperDomX. It's Basically the shell & script that will get you up and running on SuperDomX in no time flat!
+  ███████╗██╗   ██╗██████╗ ███████╗██████╗ ██████╗  ██████╗ ███╗   ███╗     ██████╗
+  ██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗████╗ ████║    ██╔═████╗
+  ███████╗██║   ██║██████╔╝█████╗  ██████╔╝██║  ██║██║   ██║██╔████╔██║    ██║██╔██║
+  ╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗██║  ██║██║   ██║██║╚██╔╝██║    ████╔╝██║
+  ███████║╚██████╔╝██║     ███████╗██║  ██║██████╔╝╚██████╔╝██║ ╚═╝ ██║    ╚██████╔╝
+  ╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝     ╚═╝     ╚═════╝
 
-    $ git clone https://github.com/XenGenie/Cloud-Strife ./SuperDom
-    $ cd ./SuperDom/x/
-    $ ./gitsub.sh
+  Versions Names By Year:
+    Year    Code Name
+    0.16.x  Neo
+    0.17.x  Morpheus
+    0.18.x  Trinity
+    0.19.x  Wizard
+    0.20.x  Priestess
+    0.21.x  Seraph
+    0.22.x  Oracle
+    0.23.x  Keymaker
+    0.24.x  Architect
+    0.25.x  Smith
+    1.16.x  Neo
+  Seasonal Realease Schedule:
+    0.xx.0 Alpha        | Spring Equinox  - March
+    0.xx.1 Beta         | Summer Solstice - June
+    0.xx.2 Delta(RC)    | Fall Equinox    - September
+    0.xx.3 Omega(Final) | Winter Solstice - December
 
-> SDX Dual-License
-> ===
-> The SDX-Kit a.k.a. Cloud-Strife is Licensed under the Artistic License 2.0.
->
-> There is a $197 fee Per Domain for our Commercial License.
+  @author @xopherdeep <heylisten@xtiv.net>
+  This program is Licensed under the Artistic License 2.0
--- x/.php
+++ x/.php
@@ -1,41 +1,9 @@
 <?php
-  /**
-    ____________________________________________________________________
-
-    ███████╗██╗   ██╗██████╗ ███████╗██████╗ ██████╗  ██████╗ ███╗   ███╗
-    ██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗████╗ ████║
-    ███████╗██║   ██║██████╔╝█████╗  ██████╔╝██║  ██║██║   ██║██╔████╔██║
-    ╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗██║  ██║██║   ██║██║╚██╔╝██║
-    ███████║╚██████╔╝██║     ███████╗██║  ██║██████╔╝╚██████╔╝██║ ╚═╝ ██║
-    ╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝     ╚═╝
-    _____________________________________________________________________
-   * x.engine.php
-   * Copyright 2014 Xopher Dee P <heylisten@xtiv.net>
-   * @TODO: Keep Being Awesome
-   *
-   *
-   * This program is free software; you can redistribute it and/or modify
-   * it under the terms of the GNU General Public License as published by
-   * the Free Software Foundation; either version 2 of the License, or
-   * (at your option) any later version.
-   *
-   * This program is distributed in the hope that it will be useful,
-   * but WITHOUT ANY WARRANTY; without even the implied warranty of
-   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   * GNU General Public License for more details.
-   *
-   * You should have received a copy of the GNU General Public License
-   * along with this program; if not, write to the Free Software
-   * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-   * MA 02110-1301, USA.
-   */
-
   // Ready.
   require("_cfg/lang.cfg");
   require("_cfg/x.cfg");
 
-  $lib = $cfg['dir']['libs'];
-
+  $lib      = $cfg['dir']['libs'];
   $engine   = "{$lib}/x4deep/xengine.php";
   $firephp  = "{$lib}/firephp/lib/FirePHPCore/FirePHP.class.php";
 
